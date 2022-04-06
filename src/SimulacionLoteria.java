c78e7857c0f999b2a92203de818f0fc6db99a63f
Josep <jcastellcolom@cifpfbmoll.eu>
2022-04-06 15:54:29 +0200

    lil fixes

--- src/SimulacionLoteria.java
+++ src/SimulacionLoteria.java
@@ -3,14 +3,14 @@ import java.util.Random;
 import exceptions.MyException;
 
 public class SimulacionLoteria {
-    public static int[] generarPrimitivaRandom(){
+    public static int[] generarPrimitivaRandom() {
         int[] primitiva = new int[6];
-        for(int i = 0; i < 6; i ++){
+        for (int i = 0; i < 6; i++) {
             boolean ok = false;
-            while(!ok){
+            while (!ok) {
                 Random rand = new Random();
                 int num = rand.nextInt(49) + 1;
-                if(!Tools.contains(primitiva, num)){
+                if (!Tools.contains(primitiva, num)) {
                     primitiva[i] = num;
                     ok = true;
                 }
@@ -19,25 +19,25 @@ public class SimulacionLoteria {
         return primitiva;
     }
 
-    public static void realizarSimulacion(){
+    public static void realizarSimulacion() {
         System.out.println("REALIZAR SIMULACIÓN");
         int times = 0;
         boolean ok = false;
-        while(!ok){
+        while (!ok) {
             System.out.println("¿Cuantas veces desa probar suerte?(Introduce un número)");
-            try{
-                if(Tools.input.hasNextInt()){
+            try {
+                if (Tools.input.hasNextInt()) {
                     times = Integer.parseInt(Tools.input.nextLine());
                     ok = true;
-                }else{
+                } else {
                     Tools.input.nextLine();
                     throw new MyException(3);
                 }
-            }catch(MyException e){
+            } catch (MyException e) {
                 System.out.println(e.getMessage());
             }
         }
-        for(int j = 0; j < App.getUsuario().getPrimitivas().size(); j++){
+        for (int j = 0; j < App.getUsuario().getPrimitivas().size(); j++) {
             System.out.println("PRIMITIVA:");
             System.out.println(App.getUsuario().getPrimitivas().get(j));
             int ceroAciertos = 0;
@@ -47,32 +47,40 @@ public class SimulacionLoteria {
             int cuatroAciertos = 0;
             int cincoAciertos = 0;
             int seisAciertos = 0;
-            for(int i = 0; i < times; i ++){
+            for (int i = 0; i < times; i++) {
                 int[] random = generarPrimitivaRandom();
                 App.getUsuario().getPrimitivas().get(j).compararPrimitiva(random);
                 System.out.println("---------------------\n");
-                System.out.println("Combinación ganadora:\n" + random[0] + "-" + random[1] + "-" + random[2] + "-" + random[3] + "-" + random[4] + "-" + random[5]);
+                System.out.println("Combinación ganadora:\n" + random[0] + "-" + random[1] + "-" + random[2] + "-"
+                        + random[3] + "-" + random[4] + "-" + random[5]);
                 System.out.println("Aciertos: " + App.getUsuario().getPrimitivas().get(j).getAciertos() + "\n");
-                switch(App.getUsuario().getPrimitivas().get(j).getAciertos()){
-                    case 0: ceroAciertos += 1;
-                    break;
-                    case 1: unAcierto += 1;
-                    break;
-                    case 2: dosAciertos += 1;
-                    break;
-                    case 3: tresAciertos += 1;
-                    break;
-                    case 4: cuatroAciertos += 1;
-                    break;
-                    case 5: cincoAciertos += 1;
-                    break;
-                    case 6: seisAciertos += 1;
-                    break;
+                switch (App.getUsuario().getPrimitivas().get(j).getAciertos()) {
+                    case 0:
+                        ceroAciertos += 1;
+                        break;
+                    case 1:
+                        unAcierto += 1;
+                        break;
+                    case 2:
+                        dosAciertos += 1;
+                        break;
+                    case 3:
+                        tresAciertos += 1;
+                        break;
+                    case 4:
+                        cuatroAciertos += 1;
+                        break;
+                    case 5:
+                        cincoAciertos += 1;
+                        break;
+                    case 6:
+                        seisAciertos += 1;
+                        break;
                 }
                 App.getUsuario().getPrimitivas().get(j).setAciertos(0);
             }
             System.out.println("0 aciertos: " + ceroAciertos);
-            System.out.println("1 acierto: " + unAcierto);
+            System.out.println("1 acierto : " + unAcierto);
             System.out.println("2 aciertos: " + dosAciertos);
             System.out.println("3 aciertos: " + tresAciertos);
             System.out.println("4 aciertos: " + cuatroAciertos);
@@ -81,10 +89,9 @@ public class SimulacionLoteria {
         }
     }
 
-
-    public static void menu(){
+    public static void menu() {
         boolean on = true;
-        while(on){
+        while (on) {
             System.out.println("-----------------------");
             System.out.println("Elige una opción:");
             System.out.println("1 - Crear Primitiva");
@@ -94,17 +101,22 @@ public class SimulacionLoteria {
 
             String opt = Tools.input.nextLine();
 
-            switch(opt){
-                case "1": Primitiva.creaPrimitiva();
-                break;
-                case "2": System.out.println(App.getUsuario());
-                break;
-                case "3": realizarSimulacion();
-                break;
-                case "0": on = false;
-                break;
-                case default: System.out.println("Introduce una de las opciones disponibles.");
-                break;
+            switch (opt) {
+                case "1":
+                    Primitiva.creaPrimitiva();
+                    break;
+                case "2":
+                    System.out.println(App.getUsuario());
+                    break;
+                case "3":
+                    realizarSimulacion();
+                    break;
+                case "0":
+                    on = false;
+                    break;
+                case default:
+                    System.out.println("Introduce una de las opciones disponibles.");
+                    break;
             }
         }
     }
